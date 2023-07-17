beb8df831c7451bfbff2eb14d544365543b42b07
sm-hdurjoy <smhazzazdurjoy@gmail.com>
2023-07-18 01:06:05 +0600

    text from input field

--- client/index.js
+++ client/index.js
@@ -1,38 +1,36 @@
-document.addEventListener('DOMContentLoaded', function (){
-  fetch('http://localhost:5000/getAll')
-  .then(response => response.json())
-  .then(data => loadHTMLTable(data['data']));
-  
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://localhost:5000/getAll")
+    .then((response) => response.json())
+    .then((data) => loadHTMLTable(data["data"]));
 });
 
-const addBTN = document.querySelector('#add-name-btn');
+const addBTN = document.querySelector("#add-name-btn");
 
-addBTN.onClick = function (){
-  const nameInput = document.querySelector('#name-input');
+addBTN.onclick = function () {
+  const nameInput = document.querySelector("#name-input");
   const name = nameInput.value;
   console.log(name);
   nameInput.value = "";
 
-  fetch('http://localhost:5000/insert',{
+  fetch("http://localhost:5000/insert", {
     headers: {
-      'content-type':'application/json'
+      "content-type": "application/json",
     },
-    method: 'POST',
-    body: JSON.stringify({name : name})
+    method: "POST",
+    body: JSON.stringify({ name: name }),
   })
-  .then(response => response.json())
-  .then(data => insertIntoRowTable(data['data']));
-}
+    .then((response) => response.json())
+    .then((data) => insertIntoRowTable(data["data"]));
+};
 
-function insertIntoRowTable (data){
+function insertIntoRowTable(data) {}
 
-}
-
-function loadHTMLTable (data){
-  const table = document.querySelector('table tbody');
+function loadHTMLTable(data) {
+  const table = document.querySelector("table tbody");
   // let tableHTML = "";
-  
-  if (data.length === 0 ){
-    table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data Found</td></tr>";
+
+  if (data.length === 0) {
+    table.innerHTML =
+      "<tr><td class='no-data' colspan='5'>No Data Found</td></tr>";
   }
 }
