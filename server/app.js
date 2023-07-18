7aaa362851440b791dd9175a4a35e44467bf6770
sm-hdurjoy <smhazzazdurjoy@gmail.com>
2023-07-18 23:57:50 +0600

    Add user name and dynamic page reload completed

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+// importing packages
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -6,19 +7,34 @@ dotenv.config();
 
 const dbService = require('./dbService');
 
+// registering  middleware functions
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Create
+// Create or insert data into database
 app.post("/insert", (request, response) => {
+  const { name } = request.body;
   console.log(request.body);
-});
 
-// Read
-app.get("/getAll", (request, response) => {
+  // Getting database instance from dbService
   const db = dbService.getDBServiceInstance();
 
+  // Calling the function to insert data into database
+  const result = db.insertNewName(name);
+
+  result
+  .then (data => response.json({ data: data}))
+  .catch (err => console.log(err));
+});
+
+// Read or getting all data from database when page loads
+app.get("/getAll", (request, response) => {
+
+  // Getting database instance from dbService
+  const db = dbService.getDBServiceInstance();
+
+  // Calling the function to get existing data from database
   const result = db.getAllData();
 
   result
--- server/dbService.js
+++ server/dbService.js
@@ -1,8 +1,10 @@
+// Importing packages and requiring them into variables
 const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
 let instance = null;
 
+// Creating Database Connection
 const connection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
@@ -11,6 +13,7 @@ const connection = mysql.createConnection({
   port: process.env.DB_PORT,
 });
 
+// Checking if connection is a success
 connection.connect((err) => {
   if (err) {
     console.log(err.message);
@@ -18,16 +21,20 @@ connection.connect((err) => {
   console.log("Database " + connection.state);
 });
 
+// Creating a database instance dynamically
 class DbService {
   static getDBServiceInstance() {
     return instance ? instance : new DbService();
   }
 
+  // Fetching all data from database when page loads
   async getAllData() {
     try {
       const response = await new Promise((resolve, reject) => {
+        // Get all data from database table query selector
         const query = "SELECT * FROM names;";
 
+        // making the query to the database to get data in result parameter
         connection.query(query, (err, result) => {
           if (err) reject(new Error(err.message));
           resolve(result);
@@ -35,6 +42,33 @@ class DbService {
       });
       // console.log(response);
       return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Insert given name into database function
+  async insertNewName(name) {
+    try {
+      const dateAdded = new Date();
+      const insertId = await new Promise((resolve, reject) => {
+        // insert given name into database table query selector
+        const query = "INSERT INTO names (name, date_added) VALUES (?,?);";
+
+        // making the query to insert name into table
+        connection.query(query, [name, dateAdded], (err, result) => {
+          // checking if there is any error
+          if (err) reject(new Error(err.message));
+          // if no error, then sending database row id back
+          resolve(result.insertId);
+        });
+      });
+      // console.log(insertId);
+      return {
+        id: insertId,
+        name: name,
+        dateAdded: dateAdded,
+      };
     } catch (error) {
       console.log(error);
     }
